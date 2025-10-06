6c96f1f6b0723de338017b3d5ad62e56c0a1d963
Go-Master-Code <kevinkristansa@gmail.com>
2025-10-06 17:33:49 +0700

    Linux VM

--- frontend/js/api/dashboard.js
+++ frontend/js/api/dashboard.js
@@ -10,45 +10,60 @@ document.addEventListener('DOMContentLoaded', () => {
 // event listener saat button find ditekan (asumsi kehadiran karyawan di tanggal tertentu)
 document.getElementById('button-find').addEventListener('click', function() {
     const nama = document.getElementById('filter-nama').value.trim();
-    const tanggal = document.getElementById('filter-tanggal').value;
+    const tanggal = document.getElementById('filter-tanggal').value.trim();
 
     if (!nama || !tanggal) {
-        alert('Mohon isi Nama Karyawan dan Tanggal');
+        showAlertFindContainer(`Mohon isi Tanggal dan Nama Karyawan!`, 'danger');
         return;
     }
 
     // Tampilkan loading atau disable button kalau perlu
     fetch(`/api/presensi/nama?nama=${nama}&tanggal=${tanggal}`)
-        .then (async res=> { // 3. tangkap error nya agar dapat dimunculkan di console
-            if (!res.ok) {
-                const errText = await res.text();
-                throw new Error(`Gagal ambil data presensi:\n${errText}`);
-            }
-            return res.json();
-        })
-        .then(data => {
-            // if (!data.success || !data.data.length) {
-            //     alert('Data absensi tidak ditemukan');
-            //     return;
-            // }
-            const presensi = data.data; // response json berupa data, jadi data.data
-            const tbody = document.querySelector('#tabel-presensi tbody');
-            tbody.innerHTML = ''; // clear dulu
+    .then (async res=> { // 3. tangkap error nya agar dapat dimunculkan di console
+        if (!res.ok) {
+            showAlertFindContainer(`Data presensi tidak ditemukan!`, 'danger');
 
-            const tr = document.createElement('tr');
-            tr.innerHTML = `
-                <td style="text-align: center;">${presensi.id}</td>
-                <td style="text-align: center;">${presensi.karyawan_id}</td>
-                <td style="text-align: center;">${presensi.karyawan_nama}</td>
-                <td style="text-align: center;">${presensi.tanggal}</td>
-                <td style="text-align: center;">${presensi.waktu_masuk}</td>
-                <td style="text-align: center;">${presensi.waktu_pulang}</td>
-                <td style="text-align: center;">${presensi.keterangan}</td>
+            const tbody = document.querySelector('#table-presensi tbody');
+            tbody.innerHTML = `
+                <tr>
+                    <td colspan="7" class="text-center text-muted">Data presensi tidak ditemukan!</td>
+                </tr>
             `;
-            tbody.appendChild(tr);
-        })
-        .catch(err => {
-            console.error(err);
-            alert('Terjadi kesalahan: ' + err.message);
-        });
+            const errText = await res.text();
+            throw new Error(`Gagal ambil data presensi:\n${errText}`);
+        }
+        return res.json();
+    })
+    .then(data => {
+        // if (!data.success || !data.data.length) {
+        //     alert('Data absensi tidak ditemukan');
+        //     return;
+        // }
+        const presensi = data.data; // response json berupa data, jadi data.data
+        const tbody = document.querySelector('#table-presensi tbody');
+        tbody.innerHTML = ''; // clear dulu
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td style="text-align: center;">${presensi.id}</td>
+            <td style="text-align: center;">${presensi.karyawan_id}</td>
+            <td style="text-align: center;">${presensi.karyawan_nama}</td>
+            <td style="text-align: center;">${presensi.tanggal}</td>
+            <td style="text-align: center;">${presensi.waktu_masuk}</td>
+            <td style="text-align: center;">${presensi.waktu_pulang}</td>
+            <td style="text-align: center;">${presensi.keterangan}</td>
+        `;
+        tbody.appendChild(tr);
+    })
+    .catch(err => {
+        console.error(err);
+        // alert('Terjadi kesalahan: ' + err.message);
     });
+});
+
+document.getElementById('button-clear').addEventListener('click', function() {
+    document.getElementById('filter-nama').value = '';
+    document.getElementById('filter-tanggal').value = '';
+    const tbody = document.querySelector('#table-presensi tbody');
+    tbody.innerHTML = '';
+});
